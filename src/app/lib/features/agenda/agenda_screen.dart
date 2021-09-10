0404a6eca1313ed1663dc6a9aabcb136cd196f35
jonatha.lima <jonatha.lima@dextra-sw.com>
2021-09-10 16:31:36 -0300

    Fix status bar color

--- src/app/lib/features/agenda/agenda_screen.dart
+++ src/app/lib/features/agenda/agenda_screen.dart
@@ -3,6 +3,7 @@ import 'package:app/features/agenda/agenda_viewmodel.dart';
 import 'package:app/widgets/card_event/card_event_view.dart';
 import 'package:design_system/widgets/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
 
 class AgendaScreen extends StatelessWidget {
@@ -18,45 +19,48 @@ class AgendaScreen extends StatelessWidget {
 
   Scaffold _buildScreen(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(8.0),
-          height: 250.0,
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Container(
-                    width: 25,
-                    child: CircleAvatar(),
-                  ),
-                  SizedBox(width: 5),
-                  AppText.heading4('Gaules'),
-                ],
-              ),
-              Flexible(
-                child: ListView(
-                  key: PageStorageKey('GaulesAgendaKey'),
-                  scrollDirection: Axis.horizontal,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 250.0,
+            child: Column(
+              children: <Widget>[
+                Row(
                   children: <Widget>[
-                    CardEventView(
-                      day: '08',
-                      month: 'out',
-                      weekday: 'seg',
-                      game: 'Counter-Strike: Global Offensive',
-                      title: 'Furia vs NIP ESL Pro League S14',
+                    Container(
+                      width: 25,
+                      child: CircleAvatar(),
                     ),
-                    CardEventView(
-                      day: '09',
-                      month: 'out',
-                      weekday: 'ter',
-                      game: 'Counter-Strike: Global Offensive',
-                      title: 'Furia vs Team Liquid ESL Pro League S14',
-                    ),
+                    SizedBox(width: 5),
+                    AppText.heading4('Gaules'),
                   ],
                 ),
-              ),
-            ],
+                Flexible(
+                  child: ListView(
+                    key: PageStorageKey('GaulesAgendaKey'),
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      CardEventView(
+                        day: '08',
+                        month: 'out',
+                        weekday: 'seg',
+                        game: 'Counter-Strike: Global Offensive',
+                        title: 'Furia vs NIP ESL Pro League S14',
+                      ),
+                      CardEventView(
+                        day: '09',
+                        month: 'out',
+                        weekday: 'ter',
+                        game: 'Counter-Strike: Global Offensive',
+                        title: 'Furia vs Team Liquid ESL Pro League S14',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- src/app/lib/features/discover/discover_screen.dart
+++ src/app/lib/features/discover/discover_screen.dart
@@ -3,6 +3,7 @@ import 'package:app/features/discover/discover_viewmodel.dart';
 import 'package:app/widgets/card_game/card_game_list_view.dart';
 import 'package:app/widgets/card_streams/card_stream_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
 
 class DiscoverScreen extends StatelessWidget {
@@ -20,21 +21,24 @@ class DiscoverScreen extends StatelessWidget {
 
   Widget _buildScreen(BuildContext context, DiscoverViewmodel viewModel) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Flexible(
-              flex: 1,
-              child: CardStreamListView(
-                hasLoader: true,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: CardStreamListView(
+                  hasLoader: true,
+                ),
               ),
-            ),
-            Flexible(
-              flex: 4,
-              child: CardGameListView(),
-            ),
-          ],
+              Flexible(
+                flex: 4,
+                child: CardGameListView(),
+              ),
+            ],
+          ),
         ),
       ),
     );
