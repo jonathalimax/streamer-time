4f8467810dc42fd00ec26c71c5ea6cf1ecf6563d
jonathalimax <jonathalimax@gmail.com>
2022-10-18 23:17:04 +0100

    Add website

--- src/design_system/lib/styles/app_colors.dart
+++ src/design_system/lib/styles/app_colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const Color kcBlack = Color(0xff262626);
 const Color kcIceWhite = Color(0xfff1f1f1);
-const Color kcPurple = Color(0xff6441a5);
+const Color kcPurple = Color(0xff6441A5);
 const Color kcLightPurple = Color(0xff9E7AE3);
 const Color kcDarkGrey = Color(0xff121212);
